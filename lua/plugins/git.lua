0c02ef5bbdcf75aca9e40ac2732f264ce0c38684
Magpiny <magpinyb@gmail.com>
2024-09-15 21:21:33 +0300

    ADDED: Neogit integration to ease working with git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -13,4 +13,18 @@ return {
 	},
 
 	{ "f-person/git-blame.nvim" },
+
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+			"echasnovski/mini.pick", -- optional
+		},
+		config = true,
+	},
 }
